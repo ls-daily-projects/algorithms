51a4253d20ba8040c7fb48317d7abf682b633b5e
Miikis <28997062+Miikis@users.noreply.github.com>
2019-10-10 10:21:38 -0700

    Finish O(n^2) solution for stock_prices problem
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,9 +2,31 @@
 
 import argparse
 
+# Time: O(n * n-1) => O(n^2)
+# Space: O(1)
+
 
 def find_max_profit(prices):
-    pass
+    prices_count = len(prices)
+
+    if (prices_count < 1):
+        return 0
+
+    if (prices_count == 1):
+        return prices[0]
+
+    max_profit = prices[1] - prices[0]
+
+    for buy_index, buy_price in enumerate(prices):
+        sell_index = buy_index + 1
+
+        while(sell_index < prices_count):
+            sell_price = prices[sell_index]
+            current_profit = sell_price - buy_price
+            max_profit = current_profit if current_profit > max_profit else max_profit
+            sell_index += 1
+
+    return max_profit
 
 
 if __name__ == '__main__':
